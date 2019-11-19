e0152872771ff28b754a871728c485bb10cd907c
Anssi Moisio <anssi.moisio@aalto.fi>
2019-11-19 18:33:55 +0200

    Added function for mel band frequencies in utils.py. Changed parameters to correspond to cakir et al (CRNN paper).

--- preprocessing_example.py
+++ preprocessing_example.py
@@ -2,16 +2,20 @@ import librosa
 from librosa import display
 import matplotlib.pyplot as plt
 import numpy as np
+import utils
 
-y, sr = librosa.load('sound_example.wav', duration=2.0)
-# y, sr = librosa.load(librosa.util.example_audio_file())
-melspec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-melspec_dB = librosa.power_to_db(melspec, ref=np.max)
+y, sr = librosa.load('sound_example.wav')
+print("librosa default sampling rate:", sr)
+melspec_dB = utils.generateMelSpecImage(y, sr)
+mfccs = utils.generateMFCCs(y, sr)
+mbe = utils.generateLogMelEnergies(y, sr)
+
+melspec_dB2 = utils.generateMelSpecImage_explicit(y, sr)
+if melspec_dB2.all() == melspec_dB.all(): print("melspec_dB1 is melspec_dB2")
 
 # plot mel spectrogram
 plt.figure(figsize=(10, 4))
-librosa.display.specshow(melspec_dB, x_axis='time',y_axis='mel', sr=sr, fmax=8000)
+librosa.display.specshow(melspec_dB, x_axis='time',y_axis='mel', sr=sr, fmax=sr)
 plt.colorbar(format='%+2.0f dB')
 plt.title('Mel-frequency spectrogram')
 plt.tight_layout()
@@ -22,4 +26,12 @@ librosa.display.specshow(mfccs, x_axis='time')
 plt.colorbar()
 plt.title('MFCCs')
 plt.tight_layout()
+
+# plot log mel band energies
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(mbe, x_axis='time',y_axis='mel', sr=sr, fmax=sr)
+plt.colorbar(format='%+2.0f dB')
+plt.title('logarithmic mel band energies')
+plt.tight_layout()
+
 plt.show()
--- utils.py
+++ utils.py
@@ -6,8 +6,37 @@ import numpy as np
 def LoadAudioFile(fileName):
     return librosa.load(fileName)
 
-def generateMelSpecImage(y, sr):
-    return librosa.power_to_db(librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000), ref=np.max)
+"""
+With the librosa default sampling rate of 22050Hz, a window of 1024 samples
+means about 46ms. This is similar with Cakir et al. 2017: "Convolutional
+Recurrent Neural Networks for Polyphonic Sound Event Detection"
+
+If unspecified, window defaults to win_length = n_fft
+"""
+def generateMelSpecImage(y, sr, n_fft=1024, hop_length=512, n_mels=40):
+    return librosa.power_to_db(librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels), ref=np.max)
+
+# the same as above but just more explicit for transparency
+def generateMelSpecImage_explicit(y, sr, n_fft=1024, hop_length=512, n_mels=40):
+    spec = np.power(np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length)), 2)
+    mel_basis = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
+    return librosa.power_to_db(np.dot(mel_basis, spec), ref=np.max)
+
+def generateLogMelEnergies(y, sr, n_fft=1024, hop_length=512, n_mels=40):
+    """
+    logarithmic mel band energies as in Cakir et al. (2017)
+
+    TODO: Cakir et al.: "each energy band is normalized by subtracting its mean and
+    dividing by its standard deviation computed over the training
+    set. The normalized log mel band energies are finally split
+    into sequences"
+    """
+    spec = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length))
+    mel_basis = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
+    return np.log(np.dot(mel_basis, spec))
+
+def generateMFCCs(y, sr, n_mfcc=40):
+    return librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
 
 def visulizeFeatures(image, sr, title='Mel-frequency spectrogram'):
     plt.figure(figsize=(10, 4))
